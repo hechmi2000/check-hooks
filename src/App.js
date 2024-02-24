dbc0b1b8890d96db82e0036eb4ae159864f28520
hechmi2000 <hechmikchouk20@gmail.com>
2024-02-24 02:07:18 +0100

    adding router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import {moviesData} from './Data';
 import { useState } from 'react';
 import AddMovie from './AddMovie/AddMovie';
 import FilterMovie from './FilterMovie/FilterMovie';
-
+import { Route, Routes } from 'react-router-dom';
+import Features from './Pages/Features';
+import Pricing  from './Pages/Pricing';
+import Errorpage from './Pages/Errorpage';
+import Description from './Pages/Description';
 function App() {
   const [movies, setMovies] = useState(moviesData)
 
@@ -19,11 +23,19 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App"> 
       <Navigation />
+      <Routes>
+        <Route path= "/Features" element={ <Features />}/>
+        <Route path= "/Pricing" element={ <Pricing />}/>
+        <Route path= "/Description/:id" element={<Description />}/>
+        <Route path= "/*" element={ <Errorpage />}/>
+      </Routes>
       <FilterMovie inputSearch={inputSearch} setInputSearch={setInputSearch} newRate={newRate} setNewRate={setNewRate}/>
       <AddMovie add={add}/>
       <MovieList movies={movies} inputSearch={inputSearch} newRate={newRate}/>
+      
+
     </div>
   );
 }
--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,16 +1,22 @@
 import Card from 'react-bootstrap/Card';
 import ReactStars from 'react-stars';
 import './MovieCard.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 function MovieCard({movie}) {
+   const navigate =useNavigate()
   return (
     <Card className='movieCard' style={{ width: '18rem' }}>
       <Card.Img className='MovieImg' variant="top" src={movie.posterUrl} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.description}</Card.Text>
+    
+        <Card.Text variant="outline-danger" onClick={()=> navigate('/Description')}>
+         {movie.description} </Card.Text>
         <Card.Text>{movie.rate} Stars</Card.Text>
         <ReactStars value={movie.rate} edit={false} size={25}/>
+        <Button variant="primary" onClick={()=> navigate(`/Description/${movie.id}`)}>More details</Button>
       </Card.Body>
     </Card>
   );
